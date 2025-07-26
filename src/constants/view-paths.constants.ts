8d64e836b8b7c42c0be2528c34e2450a7d353965
charankamal20 <charanksingh20@gmail.com>
2025-07-26 19:41:42 +0530

    add processed files

--- src/constants/view-paths.constants.ts
+++ src/constants/view-paths.constants.ts
@@ -6,7 +6,7 @@ export interface Model {
   gltf_path: string;
 }
 
-("https://kaythecosmic.github.io/zentroid-models/");
+// ("https://kaythecosmic.github.io/zentroid-models/");
 
 export const MODELS_GLTF: Record<string, Model> = {
   almirah: {
@@ -15,7 +15,8 @@ export const MODELS_GLTF: Record<string, Model> = {
     path: "https://kaythecosmic.github.io/zentroid-models/gltf-models/Almira/Almira.gltf",
     gltf_path:
       "https://zentroid.s3.eu-north-1.amazonaws.com/gltf-models/Almira/Almira.gltf",
-    aws_path: "https://zentroid.s3.eu-north-1.amazonaws.com/glb/Almira.glb",
+    aws_path:
+      "https://zentroid.s3.eu-north-1.amazonaws.com/glb/almira-processed.glb",
   },
   nike: {
     name: "nike",
@@ -23,8 +24,7 @@ export const MODELS_GLTF: Record<string, Model> = {
     path: "https://kaythecosmic.github.io/zentroid-models/gltf-models/Nike%20Air%20DT%20Max%2096%20Colorado%20Away%20Sneakers/Nike%20Air%20DT%20Max%2096%20Colorado%20Away%20Sneakers.gltf",
     gltf_path:
       "https://d3tz76b3tjibkh.cloudfront.net/gltf-models/Nike%20Air%20DT%20Max%2096%20Colorado%20Away%20Sneakers/Nike%20Air%20DT%20Max%2096%20Colorado%20Away%20Sneakers.gltf",
-    aws_path:
-      "https://d3tz76b3tjibkh.cloudfront.net/glb/Nike+Air+DT+Max+96+Colorado+Away+Sneakers.glb",
+    aws_path: "https://d3tz76b3tjibkh.cloudfront.net/glb/nike-processed.glb",
   },
   sunglasses: {
     name: "sunglasses",
@@ -32,7 +32,7 @@ export const MODELS_GLTF: Record<string, Model> = {
     path: "https://kaythecosmic.github.io/zentroid-models/gltf-models/Dior%20signature/Dior%20signature.gltf",
     gltf_path:
       "https://d3tz76b3tjibkh.cloudfront.net/gltf-models/Dior%20signature/Dior%20signature.gltf",
-    aws_path: "https://d3tz76b3tjibkh.cloudfront.net/glb/DiorSignature.glb",
+    aws_path: "https://d3tz76b3tjibkh.cloudfront.net/glb/dior-processed.glb",
   },
   sunglasses2: {
     name: "sunglasses2",
@@ -40,7 +40,8 @@ export const MODELS_GLTF: Record<string, Model> = {
     path: "https://kaythecosmic.github.io/zentroid-models/gltf-models/Clubmaster%20Way/Clubmaster%20Way.gltf",
     gltf_path:
       "https://d3tz76b3tjibkh.cloudfront.net/gltf-models/Clubmaster%20Way/Clubmaster%20Way.gltf",
-    aws_path: "https://d3tz76b3tjibkh.cloudfront.net/glb/Clubmaster+Way.glb",
+    aws_path:
+      "https://d3tz76b3tjibkh.cloudfront.net/glb/clubmaster-processed.glb",
   },
   watch: {
     name: "watch",
@@ -48,7 +49,6 @@ export const MODELS_GLTF: Record<string, Model> = {
     path: "https://kaythecosmic.github.io/zentroid-models/gltf-models/Graham%20Watch/GRAHAM%20FORTRESS%20CITY%20GMT%20RED.gltf",
     gltf_path:
       "https://d3tz76b3tjibkh.cloudfront.net/gltf-models/Graham%20Watch/GRAHAM%20FORTRESS%20CITY%20GMT%20RED.gltf",
-    aws_path:
-      "https://d3tz76b3tjibkh.cloudfront.net/glb/GRAHAM+FORTRESS+CITY+GMT+RED.glb",
+    aws_path: "https://d3tz76b3tjibkh.cloudfront.net/glb/graham-processed.glb",
   },
 };
